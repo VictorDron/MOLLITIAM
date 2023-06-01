0ccbf45b4eee503e7e3d68399477cb79b62e0eec
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2023-06-01 16:57:42 -0300

    Installation of the axios lib to consume the server's apis. Interface structuring

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,35 +1,169 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // States for storing data
+  const [polos, setPolos] = useState([]);
+  const [selectedPolo, setSelectedPolo] = useState(null);
+  const [stockValue, setStockValue] = useState(0);
+  const [dangerLevel, setDangerLevel] = useState(null);
+  const [transferPolo, setTransferPolo] = useState(null);
+  const [transferAmount, setTransferAmount] = useState(0);
+
+  // Fetch all polos on component mount
+  useEffect(() => {
+    const fetchPolos = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/polos');
+        setPolos(response.data);
+      } catch (error) {
+        console.error('Error fetching polos:', error);
+      }
+    };
+
+    fetchPolos();
+  }, []);
+
+  // Fetch danger level of selected polo
+  useEffect(() => {
+    const fetchDangerLevel = async () => {
+      if (selectedPolo) {
+        try {
+          const response = await axios.get(`http://localhost:3000/polos/search/${selectedPolo}`);
+
+          if (Array.isArray(response.data) && response.data.length > 0) {
+            setDangerLevel(response.data[0].level);
+          } else {
+            setDangerLevel(null);
+          }
+        } catch (error) {
+          console.error('Error fetching danger level:', error);
+        }
+      }
+    };
+
+    fetchDangerLevel();
+  }, [selectedPolo]);
+
+  // Event handler for selecting a polo
+  const handlePoloChange = (event) => {
+    const selectedPolo = polos.find((polo) => polo.id === parseInt(event.target.value));
+    setSelectedPolo(selectedPolo ? selectedPolo.id : null);
+    setStockValue(selectedPolo ? selectedPolo.terminal_qtd : 0);
+  };
+
+  // Event handler for adjusting the stock level of selected polo
+  const handleAdjustLevelClick = async () => {
+    if (selectedPolo) {
+      try {
+        await axios.post(`http://localhost:3000/polos/adjust/${selectedPolo}`);
+
+        const updatedPoloResponse = await axios.get(`http://localhost:3000/polos/${selectedPolo}`);
+        if (updatedPoloResponse.data) {
+          setStockValue(updatedPoloResponse.data.terminal_qtd);
+        }
+
+        fetchDangerLevel(selectedPolo);
+      } catch (error) {
+        console.error('Error on adjust level:', error);
+      }
+    }
+  };
+
+  // Event handler for selecting a polo to transfer terminals
+  const handleTransferPoloChange = (event) => {
+    const selectedPolo = polos.find((polo) => polo.id === parseInt(event.target.value));
+    setTransferPolo(selectedPolo ? selectedPolo.id : null);
+  };
+
+  // Event handler for entering the transfer amount
+  const handleTransferAmountChange = (event) => {
+    setTransferAmount(parseInt(event.target.value) || 0);
+  };
+
+  // Event handler for submitting the terminal transfer
+  const handleTransferSubmit = async (event) => {
+    event.preventDefault();
+
+    if (selectedPolo && transferPolo && transferAmount > 0) {
+      try {
+        const response = await axios.post(`http://localhost:3000/polos/expedition`, {
+          origem_id: selectedPolo,
+          destino_id: transferPolo,
+          terminal_qtd: transferAmount,
+        });
+
+        if (response.status === 200) {
+          // Update polos data after successful transfer
+          fetchPolos();
+        }
+      } catch (error) {
+        console.error('Error on transfer:', error);
+      }
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="box_1">
+        <div className="select">
+          {/* Select input for choosing a polo */}
+          <select name="" className="select_1" onChange={handlePoloChange}>
+            {polos.map((polo) => (
+              <option key={polo.id} value={polo.id}>
+                {polo.nome}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="title_stock">
+          <h1>ESTOQUE</h1>
+        </div>
+        <div className="stock_value">
+          <h1 className="stock_result">{stockValue}</h1>
+        </div>
+        <div className="title_critical">
+          <h1 className={`title_level ${dangerLevel ? dangerLevel.toLowerCase().replace(' ', '_') : ''}`}>
+            {dangerLevel}
+          </h1>
+        </div>
+        <div className="critical">
+          <div className={`critical_img ${dangerLevel ? dangerLevel.toLowerCase().replace(' ', '_') : ''}`}>
+            <img src="/logic.png" alt="Critical Image" />
+          </div>
+        </div>
+        <div className="title_expediction">
+          <h1>SELECIONE PARA EXPEDIR</h1>
+        </div>
+
+        <div className="expediction">
+          {/* Form for transferring terminals */}
+          <form onSubmit={handleTransferSubmit}>
+            <select className="select_2" onChange={handleTransferPoloChange}>
+              {polos.map((polo) => (
+                <option key={polo.id} value={polo.id}>
+                  {polo.nome}
+                </option>
+              ))}
+            </select>
+            <br />
+            <input type="text" placeholder="QUANTIDADE" onChange={handleTransferAmountChange} />
+            <br />
+            <button type="submit">ENVIAR</button>
+          </form>
+        </div>
+
+        <div className="adjust_level">
+          {/* Button for adjusting the stock level */}
+          <button onClick={handleAdjustLevelClick} className="action_level">
+            AJUSTAR COBERTURA
+          </button>
+        </div>
+        <div className="history"></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+
 // Definition of the port where the server will listen. The value is taken from the environment variables, or 3000 if not defined
 const port = process.env.PORT || 3000;
 
@@ -10,6 +12,8 @@ const polosRoutes = require('./routes/polos');
 
 // Middleware to parse the body of requests as JSON
 app.use(express.json());
+
+app.use(cors());
 
 // Setting the base routes for the imported routers
 app.use('/polos', polosRoutes);
